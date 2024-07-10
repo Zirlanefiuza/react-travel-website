f8e7b31a908140deb31bfb3cab43459b82034b0f
Zirlanefiuza <zirlanebatistafiuza@gmail.com>
2024-07-10 15:43:47 -0300

    fix: ajustes conflitos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import Banner from "./components/banner/Banner";
 import Footer from "./components/footer/Footer";
@@ -15,30 +14,6 @@ function App() {
       <Gallery />
       <Footer />
     </div>
-=======
-import ProductCard from "./studentCard/productCard/ProductCard";
-import StudentCard from "./studentCard/StudentCard";
-
-function App() {
-  return (
-    <>
-      <header>
-        <h1>REACT - Props e condicionais</h1>
-      </header>
-      <section>
-        <StudentCard name="Zirlane Fiuza" grade={9} average={8.5} />
-        <StudentCard name="Gustavo Fiuza" grade={5} average={4.0} />
-      </section>
-      <section>
-        <ProductCard
-          name="Ps5 Slim "
-          unitPrice={4399}
-          discount={14}
-        />
-        <ProductCard name="Controle" unitPrice={150} discount={0} />
-      </section>
-    </>
->>>>>>> c036e3246325996719f146f11dab7ed87d274720
   );
 }
 
